5456de8fcdd40bf9699980559c4bfa6d32e472d4
Balazs Toth <balazs.toth@balabit.com>
2019-11-08 09:49:43 +0100

    PEP8 fix
    Signed-off-by: Balazs Toth <balazs.toth@balabit.com>

--- src/main.py
+++ src/main.py
@@ -3,5 +3,6 @@ def main():
     print('Hello world')
     return 0
 
+
 if __name__ == '__main__':
     exit(main())
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,5 +1,6 @@
 
 from ..main import main
 
+
 def test_main():
     assert main() == 0
